1280fcb68cde6a663dbfe3f804036847f62ce559
Naren Sneider Correa Alvarez <sneideralv@MacBook-Pro-de-Naren.local>
2025-05-07 22:36:42 -0500

    Cambio de apuntamiento

--- client/src/app/services/product/product.service.ts
+++ client/src/app/services/product/product.service.ts
@@ -6,7 +6,8 @@ import { Product } from '../../interfaces/product';
   providedIn: 'root',
 })
 export class ProductService {
-  private API_URL = 'http://localhost:3000/api/products';
+  //private API_URL = 'http://localhost:3000/api/products';
+  private API_URL = 'http://162.240.174.55/api/products';
 
   constructor(private http: HttpClient) { }
 
